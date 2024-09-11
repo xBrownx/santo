e44c1f94143a3e2a95585f2c354a9ff97a434258
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 09:21:10 +1000

    Update and rename header.jsx to constants.jsx

---
+++ src/components/organisms/header/constants.jsx
@@ -0,0 +1,6 @@
+export const header = {
+    titalTxt: "1837 BUSINESS PARK",
+    subtitleTxt: "8-18 SANTO PARADE, PORT ADELAIDE",
+    linkTxt: ["ABOUT", "LOCATION", "SITE", "AGENCY"],
+    btnTxt: "Enquire"
+}
--- src/components/organisms/header/header.jsx
+++
@@ -1,4 +0,0 @@
-export const header = {
-    titleText: "8-18 SANTO PARADE",
-    linkText: ["About", "Location", "Site", "Agency", "Enquire"],
-}
