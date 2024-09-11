9d8ac156c87125b7db8763bc52fce6d93055307f
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 09:35:51 +1000

    Update and rename agency.jsx to constants.jsx

---
+++ src/components/pages/agency/constants.jsx
@@ -0,0 +1,32 @@
+export const agencyConst = {
+    titleTxt: "AGENCY",
+    paragraphTxt: "If you'd like to inspect this property in person, please contact an agent from McGees or RWC for further information or to arrange a private inspection.",
+    agencies: [
+        {
+            agencyName: "MCGEES PROPERTY",
+            agents: [
+                {
+                    name: "ANDREW WILSON",
+                    number: "+61 450 965 633"
+                },
+                {
+                    name: "MARCO ONORATO",
+                    number: "+61 481 157 645"
+                }
+            ]
+        },
+        {
+            agencyName: "RWC ADELAIDE",
+            agents: [
+                {
+                    name: "OLIVER TOTANI",
+                    number: "+61 412 808 743"
+                },
+                {
+                    name: "JACK DYSON",
+                    number: "+61 448 685 593"
+                }
+            ]
+        },
+        
+}
