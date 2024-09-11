32f512e39d793579cd8d3ec9e3cf826c085aa232
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 09:14:52 +1000

    Update and rename overview.jsx to constants.jsx

---
+++ src/components/pages/overview/constants.jsx
@@ -0,0 +1,23 @@
+export const overview = {
+    headingTxt: 'OVERVIEW',
+    paragraphTxt: 'RWC Adelaide & McGees Property are excited to present 1837 Business Park, a brand new office warehouse development in the absolute heart of the gentrifying Port Adelaide. Spoilt with immediate amenity nearby the recently developed 650* home master planned community - Dock One by Kite Projects.',
+    details: {
+        landArea: {
+            label: "Land Area",
+            value: "868 m²"
+        },
+        floorArea: {
+            label: "Floor Area",
+            value: "243 -868 m²"
+        },
+        internalClearance: {
+            label: "Internal Clearance",
+            value: "7.75 - 11.5 m"
+        },
+        completion: {
+            label: "Completion",
+            value: "Q2 2025"
+        },
+    },
+    buttonTxt: "FLOOR PLAN"
+}
