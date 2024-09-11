64a4ec3058b3843ace5bd90e836e1c0bc29dfb88
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 09:28:16 +1000

    Update location.jsx

--- src/components/pages/location/location.jsx
+++ src/components/pages/location/location.jsx
@@ -1,7 +1,6 @@
 export const location = {
-    headingTxt: "LOCATION",
-    subheadingTxt: "8-18 Santo Parade, Port Adelaide SA 5015",
-    paragraphTxt: "Elementum dignissim nulla volutpat etiam vel lorem auctor sed. Amet varius mauris imperdiet diam aliquet. Congue fames enim vitae et magna habitasse. Auctor proin et integer adipiscing. Porta dui mauris sit tempus.",
+    titleTxt: "LOCATION",
+    subtitleTxt: "8-18 Santo Parade, Port Adelaide SA 5015",
     buttonTxt: "VIEW ON GOOGLE MAPS",
     menu: {
         proximity: "PROXIMITY",
