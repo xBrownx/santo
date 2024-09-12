68b49dfa9b939c3cdf6bd72ed282fe4b173b76e4
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 14:42:27 +1000

    Update index.jsx

--- src/components/molecules/linkSubtitle/index.jsx
+++ src/components/molecules/linkSubtitle/index.jsx
@@ -1,9 +1,15 @@
 import { memo } from 'react';
+import { Container } from './styles';
 import { Subheading } from '../../atoms';
 
 function LinkSubtitle(props) {
     return (
-        <></>        
+        <Container>
+            <Subheading>
+                {props.children}
+            </Subheading>
+            openIcon
+        </Container>        
     );
 }
 
