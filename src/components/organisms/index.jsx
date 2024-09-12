60d29488ee50de87ef9f1bae68ea551b45a6ccc4
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 14:20:07 +1000

    Update index.jsx

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -1,5 +1,8 @@
-import Footer from "./footer";
-import Header from "./header";
-import ScrollingText from "./scrollingText";
+import AgencyAgents from './agencyAgents';
+import Footer from './footer';
+import Header from './header';
+import ScrollingText from './scrollingText';
+import WarehouseModal from './warehouseModal';
+import WarehouseOverlay from './warehouseOverlay';
 
-export { Footer, Header, ScrollingText }
+export { AgencyAgents, Footer, Header, ScrollingText, WarehouseModal, WarehouseOverlay }
