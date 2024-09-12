ccd3c19c7373b37f230404db648607494a5a7e94
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 10:19:00 +1000

    Update index.jsx

--- src/components/pages/agency/index.jsx
+++ src/components/pages/agency/index.jsx
@@ -1,28 +1,26 @@
 import { forwardRef, memo } from 'react';
 import { agency as CONST } from '../../../constants'
 import { Page } from '../../templates';
-import { PageHeading } from "../../molecules";
+import { PageTitle } from "../../molecules";
 import { Column, Paragraph } from "../../atoms";
 
 
-const Agency = memo(
-    forwardRef(
-        function () {
-            return (
-                <Page $bgPrimary>
-                    <Column gap={32}>
-                        <PageHeading $primary>
-                            {CONST.headingTxt}
-                        </PageHeading>
-                        <Paragraph size={18} lineHeight={26.94}>
-                            {CONST.paragraphTxt}
-                        </Paragraph>
-
-                    </Column>
-                </Page>
-            );
-        }
-    )
-);
+function Agency() {
+    return (
+        <Page $bgPrimary>
+            <Column gap={32}>
+                <PageTitle $primary>
+                    {CONST.titleTxt}
+                </PageTitle>
+                <Paragraph 
+                    size={18} 
+                    lineHeight={26.94}
+                >
+                    {CONST.paragraphTxt}
+                </Paragraph>
+            </Column>
+        </Page>
+    );
+}
 
 export default Agency;
