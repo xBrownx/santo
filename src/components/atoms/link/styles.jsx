5ad922e915ce306bf329f17596c11d1b0c15fd53
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 11:11:02 +1000

    Create styles.jsx

---
+++ src/components/atoms/link/styles.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import {Style as STYLE } from '../../../util';
+
+export const StyledLink = styled.a`
+    unset: all;
+    text-transform: uppercase;
+    font-size: ${STYLE.fontSize(20)};
+    color: black;
+
+    &:hover {
+        
+    }
+`
