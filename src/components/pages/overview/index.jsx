1de51acab6cdaa3069748368f6bfd855f72f9828
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 10:03:49 +1000

    Update index.jsx

--- src/components/pages/overview/index.jsx
+++ src/components/pages/overview/index.jsx
@@ -1,56 +1,38 @@
 import { memo } from 'react';
 import styled from "styled-components";
-import { overview as CONST } from "./overview.jsx";
-import { Page } from '../../templates/index.jsx';
-import { Button, Column, Container, Paragraph, Row } from "../../atoms/index.jsx";
-import { PageHeading } from "../../molecules/index.jsx";
+import { overviewConst as CONST } from "./constants.jsx";
+import { Button, Column, Container, Paragraph, SplitScreen} from "../../atoms";
+import { PageTitle } from "../../molecules";
+import { Page } from '../../templates';
 
 function Overview() {
     return (
-        <Page
+        <Page 
             $bgPrimary
             paddingInlineBlock={{block: 64}}
         >
-            <Row paddingInlineBlock={{inline: 32}}>
-                <Container width={554}>
-                    <Column gap={32}>
-                        <PageHeading $primary>
-                            {CONST.headingTxt}
-                        </PageHeading>
-
-                        <Paragraph size={18} lineHeight={26.94}>
+            <Column>
+                <SplitScreen>
+                    <Column>
+                        <PageTitle>
+                            {CONST.titleTxt}
+                        </PageTitle>
+                        <Paragraph>
                             {CONST.paragraphTxt}
                         </Paragraph>
-                        <Button $fillParent>
-                            {CONST.buttonTxt}
-                        </Button>
                     </Column>
+                    <Column>
+                        <Subtitle>
+                            {CONST.subtitleTxt}
+                        </Subtitle>
+                        <Details>
+                        </Details>
+                    </Column>
+                </SplitScreen>
+                <Container>
+                    <SwipeCarousel />
                 </Container>
-                <Container
-                    $centre
-                    widthPc={100}
-                >
-                    <Row gap={64}>
-                        <StyledUl>
-                            <li><Paragraph><b>Total Site Area:</b></Paragraph></li>
-                            <li><Paragraph><b>Total Floor Area:</b></Paragraph></li>
-                            <li><Paragraph><b>Tenure Type:</b></Paragraph></li>
-                            <li><Paragraph>14261.94 m²</Paragraph></li>
-                            <li><Paragraph>11618 m²</Paragraph></li>
-                            <li><Paragraph>Vacant Possession</Paragraph></li>
-                        </StyledUl>
-                        <StyledUl>
-                            <li><Paragraph><b>Offices:</b></Paragraph></li>
-                            <li><Paragraph><b>Units Available:</b></Paragraph></li>
-                            <li><Paragraph><b>Construction:</b></Paragraph></li>
-                            <li><Paragraph>xx</Paragraph></li>
-                            <li><Paragraph>xx</Paragraph></li>
-                            <li><Paragraph>xx</Paragraph></li>
-                        </StyledUl>
-                    </Row>
-                </Container>
-            </Row>
-            {/*    Carousel */}
+            </Column>
         </Page>
     );
 }
