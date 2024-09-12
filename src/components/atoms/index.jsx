96e49b86c058537fc8e4511849cfa6f3b7a42f7f
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 10:07:42 +1000

    Update index.jsx

--- src/components/atoms/index.jsx
+++ src/components/atoms/index.jsx
@@ -1,11 +1,13 @@
-import Button from './button'
-import ButtonSpecial from "./buttonSpecial/index.jsx";
-import Column from './column'
-import Container from './container'
+import Button from './button';
+import ButtonSpecial from "./buttonSpecial";
+import Column from './column';
+import Container from './container';
 import Heading from './Heading';
-import Input from './input'
-import Paragraph from "./paragraph/index.jsx";
-import Row from './row'
-import TextArea from './textArea'
+import Input from './input';
+import Paragraph from "./paragraph";
+import Row from './row';
+import SplitScreen from './splitScreen';
+import Subheading from './subheading';
+import TextArea from './textArea';
 
-export { Button, ButtonSpecial, Column, Container, Heading, Input, Paragraph, Row, TextArea };
+export { Button, ButtonSpecial, Column, Container, Heading, Input, Paragraph, Row, SplitScreen, Subheading, TextArea };
