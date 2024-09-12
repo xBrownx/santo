8c73442934bf96b67aac9259cb2e46a16502e6af
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 14:36:59 +1000

    Create index.jsx

---
+++ src/components/molecules/linkSubtitle/index.jsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+import { Subheading } from '../../atoms';
+
+function LinkSubtitle(props) {
+    return (
+        <></>        
+    );
+}
+
+export default memo(LinkSubtitle);
