16488de70c14508f57198fcadc7e0648b9d5d959
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-12 14:35:16 +1000

    Update index.jsx

--- src/components/organisms/agencyAgents/index.jsx
+++ src/components/organisms/agencyAgents/index.jsx
@@ -1,10 +1,26 @@
 import { memo } from 'react';
+import { Container } from './styles';
+import { Row } from '../../atoms';
+import { LinkSubtitle, Agent } from '../../molecules';
 
 function AgencyAgents(props) {
+    const { agencyName, agents } = props;
     return (
-        <>
-        
-        </>  
+        <Container>
+            <LinkSubtitle>
+                {agencyName}
+            </LinkSubtitle>
+            <Row>
+                {agents.map(agent => {
+                    return (
+                        <Agent 
+                            key={agent.id} 
+                            agent={agent}
+                        />
+                    );
+                })}
+            </Row>
+        </Container>  
     );
 }
 
