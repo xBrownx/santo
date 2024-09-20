c4f8ef033665ad640fa491f3e63934efe69718ab
xbrownx <asbrownn@gmail.com>
2024-09-20 13:10:35 +1000

    pc push

--- src/components/pages/location/constants.jsx
+++ src/components/pages/location/constants.jsx
@@ -1,4 +1,4 @@
-import locationVideo from '../../../assets/240919_8-18 Santo Parade_Location.mp4'
+import locationVideo from "../../../assets/Replika's Video - Sep 20, 2024.mp4"
 export const locationConst = {
     titleTxt: "LOCATION",
     subtitleTxt: "8-18 Santo Parade, Port Adelaide SA 5015",
