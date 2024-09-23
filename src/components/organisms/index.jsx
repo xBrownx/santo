20c651a9434791ecf850ff0387aa87074b988075
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-23 17:51:22 +1000

    Update index.jsx

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -1,8 +1,9 @@
 import AgencyAgents from './agencyAgents';
 import Footer from './footer';
 import Header from './header';
+import MpaView fro './miniMap';
 import ScrollingText from './scrollingText';
 import WarehouseModal from './warehouseModal';
 import WarehouseOverlay from './availabilityOverlay';
 
-export { AgencyAgents, Footer, Header, ScrollingText, WarehouseModal, WarehouseOverlay }
+export { AgencyAgents, Footer, Header, MapView, ScrollingText, WarehouseModal, WarehouseOverlay }
