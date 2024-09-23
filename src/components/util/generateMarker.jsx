12c09be196ea38a2a4040518150a27c2681f3538
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-23 17:55:56 +1000

    Update generateMarker.jsx

--- src/components/util/generateMarker.jsx
+++ src/components/util/generateMarker.jsx
@@ -1,6 +1,6 @@
 import { Marker, Map } from 'mapbox-gl';
 
-export const generateNewMarker = ({ lat, lng, map }: { lng: number, lat: number, map: Map }) => {
+export const generateNewMarker = ({ lat, lng, map }) => {
 
     new Marker({ color: '#63df29', scale: 1.5 })
         .setLngLat([lng, lat])
