0922b5c6d7ccb13495f1275c7ae18e7677337888
Brownx <37974829+xBrownx@users.noreply.github.com>
2024-09-23 17:56:05 +1000

    Update generateMarker.jsx

--- src/components/util/generateMarker.jsx
+++ src/components/util/generateMarker.jsx
@@ -4,6 +4,5 @@ export const generateNewMarker = ({ lat, lng, map }) => {
 
     new Marker({ color: '#63df29', scale: 1.5 })
         .setLngLat([lng, lat])
-        .setPopup(popUp)
         .addTo(map)
 }
